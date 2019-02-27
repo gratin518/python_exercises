2dea2e5758f2cb125cbd739bce6c3f78ccb46655
gratin <b1504066@planet.kanazawa-it.ac.jp>
2019-02-27 19:34:54 +0900

    commit message

---
+++ 01_execute_python/my_answer_ex01.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    print("Hello Python!")
+
+
+def plus(x1, x2):
+    return x1 + x2
+
+
+def plus_1(x):
+    one = 1
+    return one + x
+
+if __name__ == "__main__":
+    main()
+    print(plus(5, 4))
+    print(plus_1(1))
